bfe315f993167ccadd8a473fb6115944d7549af7
Niko Zurstraßen <niko.zurstrassen@rwth-aachen.de>
2022-02-28 15:53:33 +0100

    Update build.yml

--- coverage_report.bash
+++ coverage_report.bash
@@ -2,3 +2,4 @@
 mkdir -p coverage
 gcovr -r . --html --html-details --exclude tests/ -o coverage/coverage.html
 gcovr -r . --xml --print-summary --exclude tests/ -o coverage/coverage.xml # This is used for the CI
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r coverage/coverage.xml
